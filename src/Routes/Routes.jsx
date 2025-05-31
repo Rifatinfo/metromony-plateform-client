e8b0b819a6f9bdfe102059e048667139d02e4b38
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-05-31 23:38:22 +0600

    add basic setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Root from "../Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+    ],
+  },
+]);
+
+export default router;
